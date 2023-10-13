1f2dc05ad7887a82845c50576d4aaf5baf9c1646
regangiri <regangirikaruna@gmail.com>
2023-10-13 21:12:55 +0700

    feat: navbar routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <div className="px-32 py-4 flex justify-between">
-      <span className="text-xl">Home</span>
+    <div className="px-32 py-8 flex justify-between">
+      <span className="text-xl" onClick={() => router.push("/")}>
+        Home
+      </span>
       <div className=" flex justify-between">
-        <span className="text-xl mx-2">Recipe</span>
-        <span className="text-xl mx-2">Counting Akg</span>
+        <span
+          className="text-xl mx-2 cursor-pointer"
+          onClick={() => router.push("/recipe-search")}
+        >
+          Recipe
+        </span>
+        <span
+          className="text-xl mx-2 cursor-pointer"
+          onClick={() => router.push("/counting-akg")}
+        >
+          Counting Akg
+        </span>
       </div>
     </div>
   );
