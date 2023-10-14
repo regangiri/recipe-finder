8f580404f4de0dfce4778cbdd23fa02278e7104f
regangiri <regangirikaruna@gmail.com>
2023-10-14 18:18:41 +0700

    style: food detail padding

--- src/pages/recipe-detail/[id].jsx
+++ src/pages/recipe-detail/[id].jsx
@@ -9,15 +9,16 @@ const RecipeDetail = ({ foodDetail, foodRecipes }) => {
   console.log(foodRecipes);
   console.log(foodDetail);
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col py-6">
       <div className="flex">
-        <div className="flex flex-col w-1/2">
-          <span className="text-3xl font-bold mx-16">{foodDetail?.title}</span>
+        <div className="flex flex-col w-1/2 mx-16">
+          <span className="text-3xl font-bold">{foodDetail?.title}</span>
           <div className="relative w-full h-[500px]">
             <Image
               src={foodDetail?.image}
               alt=""
               fill
+              className="rounded-md"
               style={{ objectFit: "cover" }}
             />
           </div>
@@ -49,15 +50,16 @@ const RecipeDetail = ({ foodDetail, foodRecipes }) => {
             </span>
             <span className="my-4">{foodDetail?.readyInMinutes} Minutes</span>
           </div>
-          <div className="flex items-center">
-            <span className="my-4 font-semibold text-xl mr-3">
-              Total Servings:
+          <div className="flex mb-4 items-center">
+            <span className="font-semibold text-xl mr-3">Total Servings:</span>
+            <span>
+              {foodDetail?.servings}
+              {foodDetail?.servings > 1 ? " Portions" : " Portion"}
             </span>
-            <span className="my-4">{foodDetail?.servings}</span>
           </div>
         </div>
       </div>
-      <span className="mx-16 text-3xl font-semibold">How To Cook:</span>
+      <span className="mx-16 mt-4 text-3xl font-semibold">How To Cook:</span>
       {foodDetail?.analyzedInstructions?.[0]?.steps?.map((data, idx) => (
         <div className="flex ml-16 py-1" key={idx}>
           <span className="mx-3">{idx + 1}</span> <span>{data?.step}</span>
